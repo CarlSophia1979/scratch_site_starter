bd2f40406adef3e6a16115b41b340ca3a17013fe
Carl <carl@example.com>
2025-04-24 08:45:23 +0700

    Initial commit

--- run.py
+++ run.py
@@ -1,10 +1,7 @@
-from flask import Flask
+from app import create_app
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return '✅ Hello Carl, your Scratch site is working!'
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
+
